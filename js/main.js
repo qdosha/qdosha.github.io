caea31de4fdb2da74181fef05806301c88088cf9
qdosha <stefan.xx3@mail.ru>
2023-04-03 21:20:10 +0300

    03

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 let formTitel = document.querySelector('.form__titel');
 let form = document.querySelector('.form');
 
-formTitel.addEventListener('click', function() {
+formTitel.addEventListener('click', function () {
     form.classList.toggle('form--active');
+})
+
+let menu = document.querySelector('.nav_mobile');
+let menuBody = document.querySelector('.menu_mobile');
+let menuClose = document.querySelector('.menu__close');
+
+menu.addEventListener('click', function () {
+    menuBody.classList.toggle('menu_mobile--active');
+})
+
+menuClose.addEventListener('click', function () {
+    menuBody.classList.toggle('menu_mobile--active');
 })
